f620401b8124022e50f889096a1dba2f1dd318f0
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-12-21 23:28:24 -0500

    Add data file support (#3)
    * Add plugin to parse data files
    
    * Add spaces for clarity
    
    * Finish data file pipeline step
    
    * Add subdirectory data file test
    
    * Add docs on data files

---
+++ src/obelix/plugins/data.cljs
@@ -0,0 +1,29 @@
+(ns obelix.plugins.data
+  (:require ["js-yaml" :as yaml]
+            path
+            [taoensso.timbre :as log]))
+
+(def data-file? #{".json" ".yaml" ".yml"})
+
+(defn parse-data
+  [page]
+  (condp contains? (path/extname (:name page))
+    #{".json"} (-> (.parse js/JSON (:content page))
+                   (js->clj :keywordize-keys true))
+    #{".yaml" ".yml"} (-> (yaml/safeLoad (:content page))
+                          (js->clj :keywordize-keys true))))
+
+(defn data-mapper
+  [page]
+  (if (data-file? (path/extname (:name page)))
+    (assoc page
+           :type "data"
+           :data (parse-data page))
+    page))
+
+(defn plugin
+  "Parses data files"
+  [_config]
+  (fn [site-data]
+    (log/debug "Parsing data files")
+    (update site-data :routes (comp doall (partial map data-mapper)))))
--- src/obelix/plugins/output.cljs
+++ src/obelix/plugins/output.cljs
@@ -6,14 +6,17 @@
 (defn write-node
   "Writes a node in the `:routes` map into the `out` directory."
   [out node]
-  (let [output-path (path/resolve out (:name node))]
-    (log/debug "Writing output file" output-path)
-    (fs/mkdirSync (path/dirname output-path) #js {:recursive true})
-    (let [content (:content node)]
-      (if content
-        (do (fs/writeFileSync output-path content)
-            (assoc-in node [:metadata :rendered] output-path))
-        node))))
+  (if (or (not= (:type node) "data")
+          (get-in node [:metadata :render]))
+    (let [output-path (path/resolve out (:name node))]
+      (log/debug "Writing output file" output-path)
+      (fs/mkdirSync (path/dirname output-path) #js {:recursive true})
+      (let [content (:content node)]
+        (if content
+          (do (fs/writeFileSync output-path content)
+              (assoc-in node [:metadata :rendered] output-path))
+          node)))
+    node))
 
 (defn write-site
   "Writes the site in `routes` to the `out` directory."
